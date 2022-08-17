86c536b58be69bbc1f950aa098de5f59d1e5b681
kenny577 <kenneth3535@protonmail.com>
2022-08-17 22:04:24 +0300

    Move SetRoomCameraPreferencesEvent to Preferences

---
+++ Communication/Packets/Incoming/Preferences/SetRoomCameraPreferencesEvent.cs
@@ -0,0 +1,27 @@
+﻿using Plus.Database;
+using Plus.HabboHotel.GameClients;
+using Plus.Utilities;
+
+namespace Plus.Communication.Packets.Incoming.Preferences;
+
+internal class SetUserFocusPreferenceEvent : IPacketEvent
+{
+    private readonly IDatabase _database;
+
+    public SetUserFocusPreferenceEvent(IDatabase database)
+    {
+        _database = database;
+    }
+
+    public Task Parse(GameClient session, IIncomingPacket packet)
+    {
+        var focusPreference = packet.ReadBool();
+        session.GetHabbo().FocusPreference = focusPreference;
+        using var dbClient = _database.GetQueryReactor();
+        dbClient.SetQuery("UPDATE `users` SET `focus_preference` = @focusPreference WHERE `id` = @habboId LIMIT 1");
+        dbClient.AddParameter("habboId", session.GetHabbo().Id);
+        dbClient.AddParameter("focusPreference", ConvertExtensions.ToStringEnumValue(focusPreference));
+        dbClient.RunQuery();
+        return Task.CompletedTask;
+    }
+}
