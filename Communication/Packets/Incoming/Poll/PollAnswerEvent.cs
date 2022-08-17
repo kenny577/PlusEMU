2209840dbf7122586689699e406cb7c2ba1309f1
kenny577 <kenneth3535@protonmail.com>
2022-08-17 21:29:12 +0300

    Rename and add missing Poll Events

---
+++ Communication/Packets/Incoming/Poll/PollAnswerEvent.cs
@@ -0,0 +1,8 @@
+﻿using Plus.HabboHotel.GameClients;
+
+namespace Plus.Communication.Packets.Incoming.Poll;
+
+internal class PollAnswerEvent
+{
+    public Task Parse(GameClient session) => throw new NotImplementedException();
+}
