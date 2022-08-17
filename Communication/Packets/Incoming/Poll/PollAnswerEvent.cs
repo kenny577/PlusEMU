24a103370739949cb0764cad93431b6f78422b44
kenny577 <kenneth3535@protonmail.com>
2022-08-17 21:35:17 +0300

    Revert "Rename and add missing Poll Events"
    This reverts commit 2209840dbf7122586689699e406cb7c2ba1309f1.

--- Communication/Packets/Incoming/Poll/PollAnswerEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using Plus.HabboHotel.GameClients;
-
-namespace Plus.Communication.Packets.Incoming.Poll;
-
-internal class PollAnswerEvent
-{
-    public Task Parse(GameClient session) => throw new NotImplementedException();
-}
---
+++ Communication/Packets/Incoming/Rooms/Polls/PollStartEvent.cs
@@ -0,0 +1,13 @@
+﻿using Plus.Communication.Packets.Outgoing.Rooms.Polls;
+using Plus.HabboHotel.GameClients;
+
+namespace Plus.Communication.Packets.Incoming.Rooms.Polls;
+
+internal class PollStartEvent : IPacketEvent
+{
+    public Task Parse(GameClient session, IIncomingPacket packet)
+    {
+        session.Send(new PollContentsComposer());
+        return Task.CompletedTask;
+    }
+}
